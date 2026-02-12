207bae335e49e1353bc35b7098bda643d4d90b3a
atlesh <atlesh@stud.ntnu.no>
2026-02-12 12:26:34 +0100

    initial upload

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $IDATT2003.Group38;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
