542f9f8ba3a090e695e36a28c71a532eaa340949
atlesh <atlesh@stud.ntnu.no>
2026-05-14 10:12:20 +0200

    hotfix ShellController

--- src/main/java/no/ntnu/idatt2003/group38/controller/shell/ShellController.java
+++ src/main/java/no/ntnu/idatt2003/group38/controller/shell/ShellController.java
@@ -100,7 +100,7 @@ public class ShellController implements ModelObserver {
   private Page createPage(Destination destination) {
     return switch (destination) {
       case DASHBOARD -> new PlaceholderPage("Dashboard");
-      case MARKET -> new StockMarketController(this.exchange, this.player);
+      case MARKET -> new StockMarketController(this.exchange, this.player, this.shell);
       case PORTFOLIO -> new PortfolioController(this.exchange, this.player);
       case TRANSACTIONS -> new PlaceholderPage("Transactions");
       case WEEKLY_GAINERS -> new PlaceholderPage("Weekly Gainers");
