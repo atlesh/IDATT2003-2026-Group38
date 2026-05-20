6c48b64e390efd3f18f171894976983fea54b1f3
Sigurd Mjølstad-Svendsen <sigurmj@stud.ntnu.no>
2026-05-20 17:39:04 +0200

    feat: launch app from launcher view

--- src/main/java/no/ntnu/idatt2003/group38/Main.java
+++ src/main/java/no/ntnu/idatt2003/group38/Main.java
@@ -4,14 +4,14 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import no.ntnu.idatt2003.group38.controller.StartController;
-import no.ntnu.idatt2003.group38.view.StartView;
+import no.ntnu.idatt2003.group38.controller.LauncherController;
+import no.ntnu.idatt2003.group38.view.LauncherView;
 
 /**
  * Entry point of the Millions application.
  *
  * <p>Loads the Roboto font family from {@code src/main/resources/fonts}
- * and shows the {@link StartView} as the initial scene.
+ * and shows the {@link LauncherView} as the initial scene.
  */
 public class Main extends Application {
 
@@ -19,11 +19,11 @@ public class Main extends Application {
   public void start(Stage stage) {
     loadFonts();
 
-    StartView startView = new StartView();
-    new StartController(startView, stage);
+    LauncherView launcherView = new LauncherView();
+    new LauncherController(launcherView, stage);
 
-    Scene scene = new Scene(startView.getRoot(), 760, 480);
-    startView.attachTo(scene);
+    Scene scene = new Scene(launcherView.getRoot(), 760, 480);
+    launcherView.attachTo(scene);
 
     stage.setTitle("Millions");
     stage.setScene(scene);
